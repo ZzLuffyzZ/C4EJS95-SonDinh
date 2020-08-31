19454bbdc61e082f35a477282427783380e243c1
Sondinh <dnson20022001@gmail.com>
2020-08-31 22:31:43 +0700

    upadate variables's names

--- day_2/index.js
+++ day_2/index.js
@@ -1,20 +1,2 @@
-// let sum = 0;
-// for (let i = 0; i <= 10; i++) {
-//   sum += i;
-// }
-// alert(`Tong = ${sum}`);
-// let input = prompt("nhap: ");
-// if (input % 2 === 0) {
-//      alert("input is even");
-// } else {
-//      alert("input is odd");
-// }
-// let inputName = prompt("Hay nhap ten:");
-// while (!inputName) {
-//   inputName = prompt("Hay nhap lai ten: ");
-// }
-// alert(`Hi ${inputName}`);
-// let inputPass = prompt("Nhap pass: ");
-// while (inputPass.length < 6 || inputPass.length > 20) {
-//   inputPass = prompt(" Hay nhap lai pass: ");
-// }
+//Question 1
+// -"var" and "const" are variable declaration.
