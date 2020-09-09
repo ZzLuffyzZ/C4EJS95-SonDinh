f6b4411f00ddb5b0152b6f2466d5b4f9f62f020b
Son Dinh <dnson20022001@gmail.com>
2020-09-09 09:37:20 +0700

    first commit

--- self-learn Object/selfLearnObject.js
+++ self-learn Object/selfLearnObject.js
@@ -35,10 +35,37 @@
 // }
 
 //2.
-let inventory = {
-  gold: 500,
-  pouch: ["flint", "twine", "gemstone"],
-  backpack: ["xylophone", "dagger", "bedroll", "bread loaf"],
-};
-inventory.pocket = ["seashell", "strange berry", "lint"];
-console.log(inventory);
+// let inventory = {
+//   gold: 500,
+//   pouch: ["flint", "twine", "gemstone"],
+//   backpack: ["xylophone", "dagger", "bedroll", "bread loaf"],
+// };
+// //Thêm một property với tên "pocket", với value là một Array
+// //bao gồm các phần tử "seashell", "strange berry", và "lint".
+// inventory.pocket = ["seashell", "strange berry", "lint"];
+// console.log(inventory);
+// //Xóa phần tử "dagger" trong Array của property backpack.
+// inventory.backpack.splice(inventory.backpack.indexOf("dagger"), 1);
+// console.log(inventory);
+// //Cộng thêm 50 vào giá trị của gold (Giá trị sau khi cộng là 550)
+// const ADD_IN = 50;
+// inventory.gold += ADD_IN;
+// console.log(inventory.gold);
+// //Xóa toàn bộ thông tin liên quan đến pouch.
+// inventory.pouch;
+// console.log(inventory);
+
+//Mindx app
+// let movie = {
+//   title: "The dark knight rises",
+//   year: 2012,
+//   rate: 8.4,
+// };
+// console.log(movie);
+// console.log(movie.director);
+
+let favThings = ["sport", "lol", "bts", "book", "coding"];
+let [first, second, third, nearLast, last] = favThings;
+console.log(first.toUpperCase());
+console.log(nearLast.toUpperCase());
+console.log(last.toUpperCase());
