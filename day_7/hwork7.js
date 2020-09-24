fe2f1a42fc72954e675d3a11fc6c8bbeea4924b5
Sondinh <dnson20022001@gmail.com>
2020-09-24 22:47:46 +0700

    update code

--- day_7/hwork7.js
+++ day_7/hwork7.js
@@ -14,10 +14,76 @@
 //3.2.No, it can't because u can only access variables that have the same or higher scope, and variables in func is smaller than global scope
 
 //Question 4:
-let x = document.getElementsByTagName("LI").length;
-// let getTag = document.getElementsByTagName("LI");
-// document.getElementById("demo").innerHTML = getTag.length;
-// for (let i = 0; i < getTag.length; i++) {
-//   console.log(getTag[i].innerHTML);
+// let getLiTag = document.getElementsByTagName("LI");
+// console.log(getTag[0]);
+// for (let i = 0; i < getLiTag.length; i++) {
+//   console.log(getLiTag[i]);
 // }
-console.log(x);
+
+//Question 5:
+// let getDivTag = document.getElementsByTagName("div");
+// console.log(getDivTag[1]);
+// for (let i = 0; i < getDivTag.length; i++) {
+//   console.log(getDivTag[i]);
+// }
+
+//Question 6:
+// let removeElement = document.getElementById("singer-1");
+// removeElement.remove();
+
+//Question 7:
+// button1.addEventListener("click", (e) => {
+//   // Callback body
+//   console.log(e.target);
+// });
+// button2.addEventListener("click", (e) => {
+//   // Callback body
+//   console.log(e.target);
+// });
+// input1.addEventListener("keydown", (e) => {
+//   // Callback body
+//   console.log(e.key);
+// });
+//7.1.It returns the element that triggered the event. In this case the <button id="button1/button2"></button>
+//7.2. It returns the input characters.
+
+//Question 8:
+// const myInput = () => {
+//   alert("My name is Son and i want to finish studying coding");
+// };
+// myInput();
+
+//Question 9:
+// let name, wish;
+// const userInput = (name, wish) => {
+//   name = prompt("Enter your name: ");
+//   wish = prompt("Enter your wish: ");
+//   alert(`My name is ${name} and i want to ${wish}`);
+// };
+// userInput(name, wish);
+
+//Question 10:
+// let name, wish;
+// const userInput = (name, wish) => {
+//   name = prompt("Enter your name: ");
+//   wish = prompt("Enter your wish: ");
+//   const result =
+//     wish || ""
+//       ? alert(`My name is ${name} and i want to ${wish}`)
+//       : alert(`My name is ${name}`);
+//   // (wish || ""):using shorthand evaluate, mean wish !== undefined & null & ""
+// };
+// userInput(name, wish);
+
+//Question 11:
+//11.1.Write an HTML for the UI above, create and link a JS file to it
+//11.2.Get (Read) the ‘Upper it!!’ button
+const readElement = document.getElementById("upper_btn");
+console.log(readElement);
+//11.3.Catch event when ‘Upper it!!!!’ button is clicked
+upper_btn.addEventListener("click", () => {
+  console.log("Upper it!!!! just clicked");
+});
+//11.4.Get (Read) ‘Enter your name’ input
+const readElementInput = document.getElementById("name_input");
+console.log(readElementInput);
