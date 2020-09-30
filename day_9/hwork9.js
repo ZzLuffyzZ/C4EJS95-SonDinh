c381dad083a04182045694fd7b285c66c8ef1d26
Sondinh <dnson20022001@gmail.com>
2020-10-01 00:00:20 +0700

    update code

--- day_9/hwork9.js
+++ day_9/hwork9.js
@@ -27,49 +27,61 @@
 // });
 
 //Question 3:
+// const fetchPromise = fetch("https://pokeapi.co/api/v2/pokemon/pikachu/");
+// fetchPromise
+//   .then((connection) => {
+//     console.log("connected");
+//     console.log(connection);
+//     return connection.json();
+//   })
+//   .then((jsonPromise) => {
+//     console.log(jsonPromise);
+//   });
+
 //Question 4:
-const randomInRange = () => {
-  return Math.random() * 10;
-};
-const x1 = randomInRange();
-if (x1 < 0) {
-  console.log("Failed: the number is smaller than 0");
-} else if (x1 > 10) {
-  console.log("Failed: the number is bigger than 10");
-} else {
-  console.log("Passed, bravo");
-}
+// const randomInRange = () => {
+//   return Math.random() * 10;
+// };
+// const x1 = randomInRange();
+// if (x1 < 0) {
+//   console.log("Failed: the number is smaller than 0");
+// } else if (x1 > 10) {
+//   console.log("Failed: the number is bigger than 10");
+// } else {
+//   console.log("Passed, bravo");
+// }
 
 //Question 5:
-const randomNum = (a, b) => {
-  return Math.random() * (a - b) + b;
-};
-const x2 = randomNum(4, 16);
-if (x2 < 4) {
-  console.log("Failed: the number is smaller than 4");
-} else if (x2 > 16) {
-  console.log("Failed: the number is bigger than 16");
-} else {
-  console.log("Passed, bravo");
-}
+// const randomNum = (a, b) => {
+//   return Math.random() * (a - b) + b;
+// };
+// const x2 = randomNum(4, 16);
+// if (x2 < 4) {
+//   console.log("Failed: the number is smaller than 4");
+// } else if (x2 > 16) {
+//   console.log("Failed: the number is bigger than 16");
+// } else {
+//   console.log("Passed, bravo");
+// }
 
 //Question 6:
-const calcDistance = (a1, b1, a2, b2) => {
-  return Math.sqrt(Math.pow(a1 - a2, 2) + Math.pow(b1 - b2, 2));
-};
-const d = calcDistance(3, 10, 0, 6);
-if (d !== 5) {
-  console.log("Failed: the calculation is wrong");
-} else {
-  console.log("Passed, bravo");
-}
+// const calcDistance = (a1, b1, a2, b2) => {
+//   return Math.sqrt(Math.pow(a1 - a2, 2) + Math.pow(b1 - b2, 2));
+// };
+// const d = calcDistance(3, 10, 0, 6);
+// if (d !== 5) {
+//   console.log("Failed: the calculation is wrong");
+// } else {
+//   console.log("Passed, bravo");
+// }
 
 //Question 7:
-const quotesList = fetch("http://quotes.rest/qod.json");
-const {
-  contents: {
-    quotes: { quote, author },
-  },
-} = quotesList;
-
-console.log(`${quote}\n${author}`);
+const fetchData = fetch("http://quotes.rest/qod.json");
+fetchData
+  .then((jsonPromise) => {
+    return jsonPromise.json();
+  })
+  .then((quotesList) => {
+    console.log(quotesList);
+  });
+// console.log(`${quote}\n${author}`);
